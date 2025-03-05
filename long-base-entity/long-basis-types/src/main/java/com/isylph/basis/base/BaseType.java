b80e5fa8af9026bb8712db7f7320e7e3f0af04bb
denzel.lin <denzellin@users.noreply.github.com>
2025-03-06 01:17:58 +0800

    bug: BaseType没有hashcode

--- long-base-entity/long-basis-types/src/main/java/com/isylph/basis/base/BaseType.java
+++ long-base-entity/long-basis-types/src/main/java/com/isylph/basis/base/BaseType.java
@@ -1,7 +1,10 @@
 package com.isylph.basis.base;
 
+import lombok.EqualsAndHashCode;
+
 import java.io.Serializable;
 
+@EqualsAndHashCode
 public class BaseType implements Serializable {
     public BaseType() {
     }
