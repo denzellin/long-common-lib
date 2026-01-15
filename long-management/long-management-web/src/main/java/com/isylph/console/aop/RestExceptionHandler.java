b205b8e8280c89876fcfb2393b71e75116ac6c3e
denzel.lin <denzellin@users.noreply.github.com>
2026-01-15 23:03:17 +0800

    bug: enhance the exception processing

--- long-management/long-management-web/src/main/java/com/isylph/console/aop/RestExceptionHandler.java
+++ long-management/long-management-web/src/main/java/com/isylph/console/aop/RestExceptionHandler.java
@@ -1,13 +1,21 @@
 package com.isylph.console.aop;
 
+import com.isylph.basis.beans.HttpRetData;
 import com.isylph.basis.consts.BaseErrorConsts;
 import com.isylph.basis.controller.exception.RestExceptionBaseHandler;
 import com.isylph.error.model.AuxErrorMessagePO;
 import com.isylph.error.service.AuxErrorMessageService;
 import com.isylph.utils.StringUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,6 +24,23 @@ public class RestExceptionHandler extends RestExceptionBaseHandler {
 
     @Autowired
     private AuxErrorMessageService sysErrorMessageService;
+
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    protected  <T> HttpRetData<T> authenticationExceptionHandler(Exception e, HttpServletRequest request) {
+        log.warn("Exception: {}", e.getMessage());
+        return HttpRetData.error(403L, e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+    protected  <T> HttpRetData<T> generalExceptionHandler(Exception e, HttpServletRequest request) {
+        log.warn("Exception: {}", e.getMessage());
+        return HttpRetData.error(422L, e.getMessage());
+    }
 
     @Override
     protected String getMessage(Long code){
